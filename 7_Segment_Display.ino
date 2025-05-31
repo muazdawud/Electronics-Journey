ad536cb93c7e69c76d131929e48b642430c3e498
muazdawud <muazdawud307@gmail.com>
2025-05-31 19:35:10 +0100

    Arduino Projects

---
+++ 7_Segment_Display.ino
@@ -0,0 +1,180 @@
+int led[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int del = 1000;
+
+void setup() {
+  for(int i = 0; i < 8; i++){
+    pinMode(led[i], OUTPUT);
+  }
+}
+
+void loop() {
+  //To display No. 1
+  digitalWrite(led[0], LOW);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], LOW);
+  digitalWrite(led[4], LOW);
+  digitalWrite(led[5], LOW);
+  digitalWrite(led[6], LOW);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 2
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], LOW);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], HIGH);
+  digitalWrite(led[5], LOW);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 3
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], LOW);
+  digitalWrite(led[5], LOW);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 4
+  digitalWrite(led[0], LOW);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], LOW);
+  digitalWrite(led[4], LOW);
+  digitalWrite(led[5], HIGH);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 5
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], LOW);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], LOW);
+  digitalWrite(led[5], HIGH);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 6
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], LOW);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], HIGH);
+  digitalWrite(led[5], HIGH);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 7
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], LOW);
+  digitalWrite(led[4], LOW);
+  digitalWrite(led[5], LOW);
+  digitalWrite(led[6], LOW);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 8
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], HIGH);
+  digitalWrite(led[5], HIGH);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 9
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], LOW);
+  digitalWrite(led[5], HIGH);
+  digitalWrite(led[6], HIGH);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+
+  //To display No. 0
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+  digitalWrite(led[4], HIGH);
+  digitalWrite(led[5], HIGH);
+  digitalWrite(led[6], LOW);
+  digitalWrite(led[7], LOW);
+  
+  delay(del);
+
+  for(int i = 0; i < 8; i++){
+    digitalWrite(led[i], LOW);
+  }
+  //delay(del);
+}
